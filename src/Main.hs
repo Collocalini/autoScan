0c0f5ffc13659c71b8534e066c0543bb9de81727
hokum <hokum@hokum_des_eth.Dlink>
2014-06-01 21:53:07 +0400

    added 2 use case scenarios to web interface

--- src/Main.hs
+++ src/Main.hs
@@ -16,11 +16,14 @@ module Main where
 
 import           Control.Applicative
 import           Snap.Core
-import           Snap.Util.FileServe
+import           Snap.Util.FileServe()
+--import           Snap.Util.FileUploads
 import           Snap.Http.Server
 import qualified Data.ByteString.Char8 as B
 import "monads-tf" Control.Monad.Reader
 import System.Environment
+import System.Directory
+
 
 -- from this project
 import AutoScan
@@ -53,12 +56,66 @@ hasPerform (Just (p,_):_) = p == argument_perform
 ================================================================================================ --}
 site :: Snap ()
 site =
-    ifTop (writeBS $ B.pack "hello world-----") <|>
-    route [ (B.pack "demo", demoHandler)
-          , (B.pack "echo/:echoparam", echoHandler)
-          ] <|>
-    dir (B.pack "static") (serveDirectory ".")
+    ifTop (indexHandler) <|>
+    route [ (B.pack "demo_a", demo_aHandler)
+          , (B.pack "demo_b", demo_bHandler)
+          , (B.pack "submitted", writeBS $ B.pack "Please wait")
+
+          ]-- <|>
+   -- dir (B.pack "static") (serveDirectory "./static")
 ----------------------------------------------------------------------------------------------------
+
+
+indexHandler :: Snap ()
+indexHandler = do
+    index <- liftIO $ read_file_if_exists "./static/select_folder.html"
+    writeBS $ B.pack index
+
+
+  {--
+maxMb = 2
+megaByte = 2^(20::Int)
+
+myDefaultPolicy :: UploadPolicy
+myDefaultPolicy = setMaximumFormInputSize (maxMb * megaByte) defaultUploadPolicy
+
+myPerPartPolicy :: PartInfo -> PartUploadPolicy
+myPerPartPolicy _ = allowWithMaximumSize (maxMb * megaByte)
+
+
+myBusinessLogic :: MonadSnap m => PartInfo -> FilePath -> m ()
+myBusinessLogic _ p = do
+   index <- liftIO $ read_file_if_exists p
+   liftIO $ putStrLn $ "index=" ++ index
+   --writeBS $ B.pack index
+   --undefined -- whatever you actually want to do with your uploads
+
+
+myUploadHandler :: MonadSnap m =>
+                [(PartInfo, Either PolicyViolationException FilePath)] -> m ()
+myUploadHandler xs = mapM_ handleOne (filter wanted xs) where
+ wanted (_,Left _) = False
+ wanted (_,Right _) = True
+ handleOne (partInfo,Right filepath) = myBusinessLogic partInfo filepath
+
+
+myHandleFileUploads :: MonadSnap m => FilePath -> m ()
+myHandleFileUploads tempDir =
+   handleFileUploads tempDir defaultUploadPolicy  myPerPartPolicy myUploadHandler
+
+
+--}
+{--uploadFiles :: AppHandler ()
+uploadFiles = do
+  files <- handleMultipart defaultUploadPolicy $ \part -> do
+    content <-  liftM B.concat EL.consume
+    return (part, content)
+  if any (\(_,c) -> (B.length c) > 16000000) files
+    then error "One of the uploaded files is bigger then 16Mb limit size!"
+    else saveFiles files
+  syncDB
+  redirect "/files/all"--}
+
 
 
 {-- ================================================================================================
@@ -73,11 +130,26 @@ echoHandler = do
 
 
 
+readUserDir :: Maybe [B.ByteString] -> FilePath
+readUserDir Nothing = ""
+readUserDir (Just p) =  B.unpack $ B.concat p
+
+
+
+
 {-- ================================================================================================
 ================================================================================================ --}
-demoHandler :: Snap ()
-demoHandler = do
-   writeBS $ B.pack "Demo autoScan is running"
+demo_aHandler :: Snap ()
+demo_aHandler = do
+   --writeBS $ B.pack "Demo autoScan is running"
+   --d <- liftIO $ getAppUserDataDirectory "hokum"
+   --liftIO $ putStrLn d
+   cd <- getsRequest (rqParam $ B.pack "cd")
+   liftIO $ putStrLn $ show cd
+   --myHandleFileUploads "."
+   pwd <- liftIO getCurrentDirectory
+
+   liftIO $ setCurrentDirectory $ readUserDir cd
    liftIO $ autoScan $ words $ " --perform analyse,save-analysis-results,use-analysis-results " ++
     "--analysis-results-file      ./a    " ++
     "--analysis-detected-pages-to ./d    " ++
@@ -85,6 +157,43 @@ demoHandler = do
     "--white-pages-to-pdf         ./zw   " ++
     "--detected-pages-to-pdf      ./zd   " ++
     "--scans-to-pdfs              ./zp   " ++
-    "--white-page-tolerance       100"
-   writeBS $ B.pack "Demo autoScan is done"
+    "--white-page-tolerance       100    " ++
+    "--scripts-folder             " ++ pwd
+   --}
+   liftIO $ setCurrentDirectory pwd
+
+   --indexHandler
+   --writeBS $ B.pack "Demo autoScan is done"
 -----------------------------------------------------------------------------------------------------
+
+
+{-- ================================================================================================
+================================================================================================ --}
+demo_bHandler :: Snap ()
+demo_bHandler = do
+
+   cd <- getsRequest (rqParam $ B.pack "cd")
+   af <- getsRequest (rqParam $ B.pack "af")
+
+   liftIO $ putStrLn $ show cd
+   liftIO $ putStrLn $ show af
+   pwd <- liftIO getCurrentDirectory
+
+   liftIO $ setCurrentDirectory $ readUserDir cd
+   liftIO $ autoScan $ words $ " --perform read-analysis,use-analysis-results " ++
+    "--analysis-results-file      ./" ++ readUserDir af  ++ "    " ++
+    "--analysis-detected-pages-to ./d    " ++
+    "--analysis-white-pages-to    ./w    " ++
+    "--white-pages-to-pdf         ./zw   " ++
+    "--detected-pages-to-pdf      ./zd   " ++
+    "--scans-to-pdfs              ./zp   " ++
+    "--white-page-tolerance       100    "
+
+   --}
+   liftIO $ setCurrentDirectory pwd
+
+   --indexHandler
+   --writeBS $ B.pack "Demo autoScan is done"
+-----------------------------------------------------------------------------------------------------
+
+
